8ce591651539c81e23c56ca7467838ef6bba39f8
ahmedelarabykhaled <ahmedelarabykhaled2020@yahoo.com>
2023-04-29 06:28:31 +0300

    add dimmers

--- resources/views/vendor/voyager/dimmers.blade.php
+++ resources/views/vendor/voyager/dimmers.blade.php
@@ -1,24 +1,89 @@
 @php
-$dimmerGroups = Voyager::dimmers();
+
 @endphp
 
-@if (count($dimmerGroups))
-@foreach($dimmerGroups as $dimmerGroup)
-    @php
-    $count = $dimmerGroup->count();
-    $classes = [
-        'col-xs-12',
-        'col-sm-'.($count >= 2 ? '6' : '12'),
-        'col-md-'.($count >= 3 ? '4' : ($count >= 2 ? '6' : '12')),
-    ];
-    $class = implode(' ', $classes);
-    $prefix = "<div class='{$class}'>";
-    $surfix = '</div>';
-    @endphp
-    @if ($dimmerGroup->any())
-    <div class="clearfix container-fluid row">
-        {!! $prefix.$dimmerGroup->setSeparator($surfix.$prefix)->display().$surfix !!}
+
+<div class="clearfix container-fluid row">
+    {{-- @can('browse', App\Customer::class) --}}
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="panel widget center bgimage"
+            style="margin-bottom:0;overflow:hidden;background-image:url('{{ asset('images/clients.jpg') }}');">
+            <div class="dimmer"></div>
+            <div class="panel-content">
+                <i class="voyager-file-text"></i>
+                <h1 style="color: white;">{{ \App\Customer::get()->count() }}</h1>
+                <h4>
+                    عميل
+                </h4>
+                {{-- <p>لديك 2 صفحات في قاعدة البيانات الخاصة بك. انقر على الزر أدناه لعرض جميع الصفحات.</p> --}}
+                {{-- <a href="http://localhost/bsmart/public/admin/pages" class="btn btn-primary">عرض جميع العملاء</a> --}}
+            </div>
+        </div>
     </div>
-    @endif
-@endforeach
-@endif
+    {{-- @endcan --}}
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="panel widget center bgimage"
+            style="margin-bottom:0;overflow:hidden;background-image:url('{{ asset('images/visitor.jpg') }}');">
+            <div class="dimmer"></div>
+            <div class="panel-content">
+                <i class="voyager-file-text"></i>
+                <h1 style="color: white;">{{ \App\Visitor::get()->count() }}</h1>
+                <h4>
+                    زائر
+                </h4>
+                {{-- <a href="http://localhost/bsmart/public/admin/pages" class="btn btn-primary">عرض جميع الزوار</a> --}}
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="panel widget center bgimage"
+            style="margin-bottom:0;overflow:hidden;
+            background-image:url('{{ asset('images/installation_request.jpg') }}');">
+            <div class="dimmer"></div>
+            <div class="panel-content">
+                <i class="voyager-file-text"></i>
+                <h1 style="color: white;">{{ \App\CustomerInstallmentRequest::get()->count() }}</h1>
+                <h4>
+                     طلب تقسيط</h4>
+                {{-- <a href="http://localhost/bsmart/public/admin/pages" class="btn btn-primary">عرض جميع طلبات التقسيط</a> --}}
+            </div>
+        </div>
+    </div><div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="panel widget center bgimage"
+            style="margin-bottom:0;overflow:hidden;background-image:url('{{ asset('images/traders.jpg') }}');">
+            <div class="dimmer"></div>
+            <div class="panel-content">
+                <i class="voyager-file-text"></i>
+                <h1 style="color: white;">{{ \App\Trader::get()->count() }}</h1>
+                <h4>
+                    تاجر</h4>
+                {{-- <a href="http://localhost/bsmart/public/admin/pages" class="btn btn-primary">عرض جميع التجار</a> --}}
+            </div>
+        </div>
+    </div><div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="panel widget center bgimage"
+            style="margin-bottom:0;overflow:hidden;background-image:url('{{ asset('images/customer_request.jpeg') }}');">
+            <div class="dimmer"></div>
+            <div class="panel-content">
+                <i class="voyager-file-text"></i>
+                <h1 style="color: white;">{{ \App\CustomerRequest::get()->count() }}</h1>
+                <h4>
+                     طلب عميل
+                </h4>
+                {{-- <a href="http://localhost/bsmart/public/admin/pages" class="btn btn-primary">عرض جميع طلبات العملاء</a> --}}
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="panel widget center bgimage"
+            style="margin-bottom:0;overflow:hidden;background-image:url('{{ asset('images/steps_actions.jpg') }}');">
+            <div class="dimmer"></div>
+            <div class="panel-content">
+                <i class="voyager-file-text"></i>
+                <h1 style="color: white;">{{ \App\StepsAction::get()->count() }} </h1>
+                <h4>steps & action</h4>
+                {{-- <a href="http://localhost/bsmart/public/admin/pages" class="btn btn-primary">عرض جميع steps & actions</a> --}}
+            </div>
+        </div>
+    </div>
+</div>
